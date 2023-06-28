e9c0d6bac93315c9499e7d4327910a53a432aadd
Altro31 <albe020531@outlook.com>
2023-06-27 20:07:20 -0400

    Singleton y comenté los métodos Set del árbol y el grafo

--- Bookcase/principal/classes/Bookcase.java
+++ Bookcase/principal/classes/Bookcase.java
@@ -2,49 +2,64 @@ package classes;
 
 import cu.edu.cujae.ceis.graph.LinkedGraph;
 import cu.edu.cujae.ceis.graph.interfaces.ILinkedNotDirectedGraph;
+import cu.edu.cujae.ceis.tree.binary.BinaryTreeNode;
 import cu.edu.cujae.ceis.tree.general.GeneralTree;
 import cu.edu.cujae.ceis.tree.iterators.general.InDepthIterator;
 
-public class Bookcase {
-	
+public class Bookcase implements NodeInfo {
+
+	// General Tree
 	private GeneralTree<NodeInfo> tree;
+
+	// Not Directed Graph
 	private ILinkedNotDirectedGraph graph;
-	
-	public Bookcase(GeneralTree<NodeInfo> tree, LinkedGraph graph) {
-		this.tree = tree;
-		this.graph = graph;
+
+	// Singleton
+	private Bookcase instance;
+
+	// Constructors
+	private Bookcase() {
+		tree = new GeneralTree<NodeInfo>();
+		graph = new LinkedGraph();
 	}
+
+	public Bookcase getInstance() {
+		if (instance == null) {
+			instance = new Bookcase();
+			instance.tree.setRoot(new BinaryTreeNode<NodeInfo>(instance));
+		}
+		return instance;
+	}
+
 	public GeneralTree<NodeInfo> getTree() {
 		return tree;
 	}
+//
+//	public void setTree(GeneralTree<NodeInfo> tree) {
+//		this.tree = tree;
+//	}
 
-	public void setTree(GeneralTree<NodeInfo> tree) {
-		this.tree = tree;
-	}
-	
-	//test funtion para que naylet vea 
-	public Carreer getFirstCarreer()
-	{
-		InDepthIterator<NodeInfo> iter= tree.inDepthIterator();
-		
-		Carreer carrear_esc = new Carreer();
-		boolean esc = true;
-		while (esc && iter.hasNext()) {
+	// test funtion para que naylet vea
+	public Carreer getFirstCarreer() {
+		InDepthIterator<NodeInfo> iter = tree.inDepthIterator();
+
+		Carreer carrear_esc = null;
+		while (carrear_esc == null && iter.hasNext()) {
 			NodeInfo aux = iter.next();
 			if (aux instanceof Carreer) {
-				esc = false;
-				carrear_esc = (Carreer)aux;
+				carrear_esc = (Carreer) aux;
 			}
-			
+
 		}
-		
+
 		return carrear_esc;
 	}
-		
+
 	public ILinkedNotDirectedGraph getGraph() {
 		return graph;
 	}
-	public void setGraph(ILinkedNotDirectedGraph graph) {
-		this.graph = graph;
-	}
+
+//	public void setGraph(ILinkedNotDirectedGraph graph) {
+//		this.graph = graph;
+//	}
 }
