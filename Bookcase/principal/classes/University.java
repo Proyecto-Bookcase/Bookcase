ae3e204e6fdee468f4222bcf51d40bdac7fa3c66
Altro31 <albe020531@outlook.com>
2023-06-28 01:50:09 -0400

    Creada la clase University

---
+++ Bookcase/principal/classes/University.java
@@ -0,0 +1,20 @@
+package classes;
+
+public class University implements NodeInfo{
+	
+	private String id;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	private void setId(String id) {
+		this.id = id;
+	}
+}
