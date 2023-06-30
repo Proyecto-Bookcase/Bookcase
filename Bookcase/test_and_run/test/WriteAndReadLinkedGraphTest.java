7776d074c0ccc855008f6682dabdc3da89f93dd8
Altro31 <albe020531@outlook.com>
2023-06-29 21:58:43 -0400

    Test para Lectura y Escritura de un Grafo

---
+++ Bookcase/test_and_run/test/WriteAndReadLinkedGraphTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.io.File;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cu.edu.cujae.ceis.graph.LinkedGraph;
+import cu.edu.cujae.ceis.graph.interfaces.ILinkedNotDirectedGraph;
+import externalMemory.ReadLinkedGraph;
+import externalMemory.WriteLinkedGrah;
+
+class WriteAndReadLinkedGraphTest {
+
+    private ILinkedNotDirectedGraph graph;
+    private File filePath;
+    private File indexPath;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        
+        filePath = new File("Graph.DATA");
+        indexPath = new File("Index.DATA");
+        
+        graph = new LinkedGraph();
+
+        graph.insertVertex("A");
+        graph.insertVertex("B");
+        graph.insertVertex("C");
+        graph.insertVertex("D");
+
+        graph.insertEdgeNDG(0, 1);
+        graph.insertEdgeNDG(0, 2);
+
+        graph.insertEdgeNDG(1, 2);
+
+        graph.insertEdgeNDG(1, 3);
+    }
+
+    @Test
+    void test() {
+        WriteLinkedGrah writer =  new WriteLinkedGrah(filePath, indexPath, (LinkedGraph)graph);
+        writer.writeGraph();
+        
+        new ReadLinkedGraph(filePath, indexPath);
+    }
+
+}
