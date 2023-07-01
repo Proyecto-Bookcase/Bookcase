2cbcb368ae87423a25bfd5c78a1ae0717b872ac6
naybv03 <nayletbv03@gmail.com>
2023-07-01 18:34:28 -0400

    csoas de las visual

---
+++ Bookcase/visual/logica/Validacion.java
@@ -0,0 +1,18 @@
+package logica;
+
+public class Validacion {
+
+	/**TODO
+	 * hace falta hacer un metodo para ver si es un estudainte(false) o un profesor(true) 
+	 * 
+	 */
+	public boolean isProfesor(String profesor) {
+		return true;
+	}
+	/**TODO
+	 * validar que el usuario y la contrasena sean correctos
+	 */
+	public boolean isCorrectUsernamePasword(String user, String pasword) {
+		return true;
+	}
+}
