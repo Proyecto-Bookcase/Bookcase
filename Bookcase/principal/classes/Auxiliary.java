e2e086697519fd4f434130dcc0662c653c5d27ce
Dialcr <dialcr45@gmail.com>
2023-07-01 19:35:07 -0400

    Fixed

--- Bookcase/principal/classes/Auxiliary.java
+++ Bookcase/principal/classes/Auxiliary.java
@@ -1,31 +1,25 @@
 package classes;
 
-import cu.edu.cujae.ceis.tree.binary.BinaryTree;
 import cu.edu.cujae.ceis.tree.binary.BinaryTreeNode;
 
 public class Auxiliary {
-    private BinaryTree<NodeInfo> carrerNode;
-    private BinaryTreeNode<NodeInfo> yearNode;
-    private BinaryTreeNode<NodeInfo> subjectNode;
-    
-    public BinaryTree<NodeInfo> getCarrerNode() {
-        return carrerNode;
-    }
-    public void setCarrerNode(BinaryTree<NodeInfo> carrerNode) {
-        this.carrerNode = carrerNode;
-    }
-    public BinaryTreeNode<NodeInfo> getYearNode() {
-        return yearNode;
-    }
-    public void setYearNode(BinaryTreeNode<NodeInfo> yearNode) {
-        this.yearNode = yearNode;
-    }
-    public BinaryTreeNode<NodeInfo> getSubjectNode() {
-        return subjectNode;
-    }
-    public void setSubjectNode(BinaryTreeNode<NodeInfo> subjectNode) {
-        this.subjectNode = subjectNode;
-    }
+	private BinaryTreeNode<NodeInfo> carrerNode;
+	private BinaryTreeNode<NodeInfo> yearNode;
 
+	public BinaryTreeNode<NodeInfo> getCarrerNode() {
+		return carrerNode;
+	}
+
+	public void setCarrerNode(BinaryTreeNode<NodeInfo> carrerNode) {
+		this.carrerNode = carrerNode;
+	}
+
+	public BinaryTreeNode<NodeInfo> getYearNode() {
+		return yearNode;
+	}
+
+	public void setYearNode(BinaryTreeNode<NodeInfo> yearNode) {
+		this.yearNode = yearNode;
+	}
 
 }
