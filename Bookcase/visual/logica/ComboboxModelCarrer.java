60b9ec843a7e2f044eb7fbdef673f55c8ab680b1
Dialcr <dialcr45@gmail.com>
2023-07-04 16:28:17 -0400

    models de combobox para la busqueda avanzada

--- Bookcase/visual/logica/ComboboxModelCarrer.java
+++ Bookcase/visual/logica/ComboboxModelCarrer.java
@@ -1,13 +1,20 @@
 package logica;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.swing.DefaultComboBoxModel;
 
 import classes.Carreer;
 
 public class ComboboxModelCarrer extends DefaultComboBoxModel<String>{
+	private LinkedList<Carreer> carreraComboList;
+	private int seletion;
 	
-	public ComboboxModelCarrer(String[] items) {
+	public ComboboxModelCarrer(String[] items,LinkedList<Carreer> carreraComboList) {
         super(items);
+        this.carreraComboList = carreraComboList;
+        setSeletion(0);
     }
 
     @Override
@@ -15,6 +22,20 @@ public class ComboboxModelCarrer extends DefaultComboBoxModel<String>{
         String item = (String) super.getElementAt(index);
         return item;
     }
+    
+    public Carreer getelemCarreer(int index)
+    {
+    	return carreraComboList.get(index);
+    }
+
+	public int getSeletion() {
+		return seletion;
+	}
+
+	public void setSeletion(int seletion) {
+		this.seletion = seletion;
+	}
+    
 
 
 }
--- Bookcase/visual/logica/ComboboxModelYear.java
+++ Bookcase/visual/logica/ComboboxModelYear.java
@@ -1,11 +1,23 @@
 package logica;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.swing.DefaultComboBoxModel;
 
+import classes.Bookcase;
+import classes.Carreer;
+import classes.Year;
+
 public class ComboboxModelYear extends DefaultComboBoxModel<String>{
+	private List<Year> comboYearList;
+	private int seletion;
 	
 	public ComboboxModelYear(String[] items) {
         super(items);
+        comboYearList = new LinkedList<Year>();
+        setSeletion(0);
     }
 
     @Override
@@ -13,6 +25,28 @@ public class ComboboxModelYear extends DefaultComboBoxModel<String>{
         String item = (String) super.getElementAt(index);
         return item;
     }
+    public Year getelemYear(int index)
+    {
+    	return comboYearList.get(index);
+    }
+    public void update(Carreer carreer,Bookcase bookcase)
+    {
+    	removeAllElements();
+    	comboYearList = bookcase.getAllYearOfCarrer(carreer);
+    	Iterator<Year> iter = comboYearList.iterator();
+    	while (iter.hasNext()) {
+    		addElement(Integer.toString(iter.next().getNumberYear()));
+			
+		}
+    }
+
+	public int getSeletion() {
+		return seletion;
+	}
+
+	public void setSeletion(int seletion) {
+		this.seletion = seletion;
+	}
 
 
 }
